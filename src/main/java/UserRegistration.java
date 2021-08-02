10e698afe0a12f93ad3ca0680fe326ecbb459526
kaviyasree99 <kaviyarajalingam@gmail.com>
2021-08-02 22:10:36 +0530

    ADDED

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -3,16 +3,19 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
+    /*
+     FirstName Validation method
+     */
     public static boolean firstNameValidation(String FirstName) {
-        // String regex = "^[A-Z]{1}[a-z A-Z]{2,}$";
-        //Pattern p = Pattern.compile(regex);
-        //if (FirstName == null){
-        ///   return false;
-        //  }
-        // Matcher m = p.matcher(FirstName);
-        // return m.matches();
         boolean validation = Pattern.matches("^[A-Z][a-z]{2,}", FirstName);
+        return validation;
+    }
 
+    /*
+      LastName Validation method
+     */
+    public static boolean lastNameValidation(String LastName) {
+        boolean validation = Pattern.matches("^[A-Z][a-z]{2,}", LastName);
         return validation;
     }
 
@@ -20,8 +23,8 @@ public class UserRegistration {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Registration System");
         Scanner input = new Scanner(System.in);
-        boolean validName;
 
+        //FirstName
         System.out.print("Please Enter firstname: ");
         String FirstName = input.nextLine();
         if (firstNameValidation(FirstName)) {
@@ -30,5 +33,13 @@ public class UserRegistration {
             System.out.println(FirstName + " is Invalid name");
         }
 
+        //LastName
+        System.out.println("Please Enter your lastname: ");
+        String LastName = input.nextLine();
+        if (lastNameValidation(LastName)) {
+            System.out.println(LastName + " is Valid name");
+        } else {
+            System.out.println(LastName + " is Invalid name");
+        }
     }
 }
